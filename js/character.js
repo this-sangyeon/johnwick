ce4184fc9a68cd8979b8070e4d409bd284578850
this-sangyeon <92290951+this-sangyeon@users.noreply.github.com>
2021-11-07 19:29:39 +0900

    .

--- js/character.js
+++ js/character.js
@@ -1,47 +1,48 @@
-let jwNavLink = document.querySelectorAll('.jw-nav ul > li > a');
-jwNavLink = Array.prototype.slice.call(jwNavLink);
-const clipBox = document.querySelector('.box');
-let bgImages = document.querySelectorAll('.bg-imgs');
+// const clipBox = document.querySelector('.box');
+// let bgImages = document.querySelectorAll('.bg-imgs');
 
-let section = document.querySelectorAll('section');
-const bgSection = document.querySelector('.character-container > .bg-section');
-const navWrap = document.querySelector('.nav-wrap');
+// let section = document.querySelectorAll('section');
+// const bgSection = document.querySelector('.character-container > .bg-section');
+// const navWrap = document.querySelector('.nav-wrap');
 
 
-for(let i =0; i< jwNavLink.length; i++){
-    jwNavLink[i].addEventListener('click',()=>{
-        // e.defaultPrevented();
-        for(let j = 0; j < jwNavLink.length; j++){
-            jwNavLink[j].classList.remove('active');
-        }
-        jwNavLink[i].classList.add('active');
-    })
-}
+// for(let i =0; i< jwNavLink.length; i++){
+//     jwNavLink[i].addEventListener('click',()=>{
+//         // e.defaultPrevented();
+//         for(let j = 0; j < jwNavLink.length; j++){
+//             jwNavLink[j].classList.remove('active');
+//         }
+//         jwNavLink[i].classList.add('active');
+//     })
+// }
 
-jwNavLink[1].addEventListener('click',()=>{
-    section[1].classList.add('active');
-    setTimeout(()=>{
-        clipBox.classList.add('active');
-        bgSection.classList.add('active');
-        navWrap.classList.add('active');
-    },700)
+// jwNavLink[1].addEventListener('click',()=>{
+//     section[1].classList.add('active');
+//     setTimeout(()=>{
+//         clipBox.classList.add('active');
+//         bgSection.classList.add('active');
+//         navWrap.classList.add('active');
+//     },700)
     
    
     
-})
-jwNavLink[0].addEventListener('click',()=>{
-    setTimeout(()=>{
-        clipBox.classList.remove('active');
-        bgSection.classList.remove('active');
-        navWrap.classList.remove('active');
-    },700)
+// })
+// jwNavLink[0].addEventListener('click',()=>{
     
+//     setTimeout(()=>{
+//         clipBox.classList.remove('active');
+//         bgSection.classList.remove('active');
+//         navWrap.classList.remove('active');
+//     },700)
+//     setTimeout(()=>{
+//         section[1].classList.remove('active');
+//     },800)
  
     
 
-})
-jwNavLink[2].addEventListener('click',()=>{
-    clipBox.classList.remove('active');
-    bgSection.classList.remove('active');
-    section[1].classList.remove('active');
-})
+// })
+// jwNavLink[2].addEventListener('click',()=>{
+//     clipBox.classList.remove('active');
+//     bgSection.classList.remove('active');
+//     section[1].classList.remove('active');
+// })
---
+++ js/mainpage.js
@@ -0,0 +1,76 @@
+let jwNavLink = document.querySelectorAll('.jw-nav ul > li > a');
+let section = document.querySelectorAll('section');
+const chapterPrevBtn = document.querySelector('.chapter-arrow > .prev');
+const chapterNextBtn = document.querySelector('.chapter-arrow > .next')
+let bgImages = document.querySelectorAll('.main-container > .bg-section > .bg-imgs');
+let mainIntroArea = document.querySelectorAll('.main-content > .main-intro');
+let introLength = mainIntroArea.length;
+let currentIndex = 0;
+
+
+// 트레일러
+const trailerBtn = document.querySelector('.trailer-btn');
+const trailerArea = document.querySelector('.trailer-area');
+const closeBtn = document.querySelector('.close-btn');
+let infoBtn = document.querySelectorAll('.InfoBtn > button')
+
+
+jwNavLink = Array.prototype.slice.call(jwNavLink);
+bgImages = Array.prototype.slice.call(bgImages);
+mainIntroArea = Array.prototype.slice.call(mainIntroArea);
+
+
+// 메인 네비게이션
+for(let i =0; i< jwNavLink.length; i++){
+    jwNavLink[i].addEventListener('click',()=>{
+        // e.defaultPrevented();
+        for(let j = 0; j < jwNavLink.length; j++){
+            jwNavLink[j].classList.remove('active');
+        }
+        jwNavLink[i].classList.add('active');
+    })
+}
+// 트레일러
+trailerBtn.addEventListener('click', ()=>{
+    console.log('click');
+    trailerArea.classList.add('active');
+    trailerBtn.classList.add('active');
+})
+
+closeBtn.addEventListener('click', ()=>{
+    console.log('click');
+    trailerArea.classList.remove('active');
+    setTimeout(()=>{
+        trailerBtn.classList.remove('active');
+    },700)
+})
+
+for(let i = 0; i <infoBtn.length; i++){
+    infoBtn[i].addEventListener('mouseenter',()=>{
+        infoBtn[i].style.background = "#4F0BC1"
+        infoBtn[i].style.transition = "0.3s ease-in"
+    })
+    infoBtn[i].addEventListener('mouseleave',()=>{
+        infoBtn[i].style.background = "none"
+        infoBtn[i].style.transition = "0.3s ease-in"
+    })
+}
+
+chapterPrevBtn.addEventListener('click',()=>{
+    currentIndex = currentIndex - 1;
+    currentIndex < 0 && (currentIndex = introLength - 1);
+    console.log(currentIndex);
+    for(let i = 0; i < mainIntroArea.length; i++){
+        mainIntroArea[i].classList.remove('active');
+    }
+    mainIntroArea[currentIndex].classList.add('active');
+})
+chapterNextBtn.addEventListener('click',()=>{
+    currentIndex = currentIndex + 1;
+    currentIndex > introLength - 1 && (currentIndex = 0);
+    console.log(currentIndex);
+    for(let i = 0; i < mainIntroArea.length; i++){
+        mainIntroArea[i].classList.remove('active');
+    }
+    mainIntroArea[currentIndex].classList.add('active');
+})
