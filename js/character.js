bd3ef936839c368716eb9a273cd9288ade32d4fb
this-sangyeon <92290951+this-sangyeon@users.noreply.github.com>
2021-11-16 22:51:27 +0900

    character img array

--- js/character.js
+++ js/character.js
@@ -3,27 +3,28 @@ const bgSection = document.querySelector('.character-container > .bg-section');
 let characterBgImgAll = document.querySelectorAll('.character-container .bg-section .bg-imgs > .bg-imgsWrap');
 const navWrap = document.querySelector('.nav-wrap');
 const characterContent = document.querySelector('.character-content');
-const bigimgContainer = document.querySelector('.bigimg-container > .img-popup-area');
+// const bigimgContainer = document.querySelector('.bigimg-container > .img-popup-area');
 let characterIntro = document.querySelectorAll('.character-content > .character-intro');
+let cutWrapsList = document.querySelectorAll('.still-list');
 let cutWraps = document.querySelectorAll('.cutWrap');
-const cutWrapImg = document.querySelectorAll('.cutWrap > .cut-container > div');
-const imgPopupEl = document.querySelectorAll('.img-popup');
-const imgPopupElImg = document.querySelectorAll('.img-popup > img');
+let cutWrapImg = document.querySelectorAll('.cutWrap > figure > img');
+let imgPopupEl = document.querySelectorAll('.img-popup-area > .img-popup');
+let imgPopupElImg = document.querySelectorAll('.img-popup > img');
 const characterNavEls = document.querySelectorAll('.character-nav > ul > li ');
-// const imgPopupElLink =  [
-//     {
-//         img:['img/character/johnwick/johnwick1.jpg', 'img/character/johnwick/johnwick1.jpg', 'img/character/johnwick/johnwick1.jpg', 'img/character/johnwick/johnwick1.jpg']
-//     },
-//     {
-//         img:['img/character/sofia/sofia1.jpg', 'img/character/sofia/sofia2.jpg', 'img/character/sofia/sofia3.jpg', 'img/character/sofia/sofia3.jpg']
-//     },
-//     {
-//         img:['img/character/winston/winston1.jpg', 'img/character/winston/winston2.jpg', 'img/character/winston/winston3.jpg', 'img/character/winston/winston2.jpg']
-//     },
-//     {
-//         img:['img/character/johnwick/johnwick1.jpg', 'img/character/johnwick/johnwick1.jpg', 'img/character/johnwick/johnwick1.jpg', 'img/character/johnwick/johnwick1.jpg']
-//     }
-// ];
+const imgPopupElLink =  [
+    {
+        img:['img/character/johnwick/johnwick1.jpg', 'img/character/johnwick/johnwick1.jpg', 'img/character/johnwick/johnwick1.jpg', 'img/character/johnwick/johnwick1.jpg']
+    },
+    {
+        img:['img/character/sofia/sofia1.jpg', 'img/character/sofia/sofia2.jpg', 'img/character/sofia/sofia3.jpg', 'img/character/sofia/sofia3.jpg']
+    },
+    {
+        img:['img/character/winston/winston1.jpg', 'img/character/winston/winston2.jpg', 'img/character/winston/winston3.jpg', 'img/character/winston/winston2.jpg']
+    },
+    {
+        img:['img/character/johnwick/johnwick1.jpg', 'img/character/johnwick/johnwick1.jpg', 'img/character/johnwick/johnwick1.jpg', 'img/character/johnwick/johnwick1.jpg']
+    }
+];
 
 // const PopupLinks = imgPopupElLink.filter(PopupLink => PopupLink.img);
 
@@ -35,19 +36,20 @@ const characterNavEls = document.querySelectorAll('.character-nav > ul > li ');
 // imgPopup.dataset.type // img
 
 
-
+ 
 
 // 해야할 것: 캐릭터 내의 네비게이션 눌렀다가 홈이나 갤러리 눌렀을 때 네비게이션 비활성화
 let popUpIndex = 0;
 jwNavLink = Array.prototype.slice.call(jwNavLink);
 characterIntro = Array.prototype.slice.call(characterIntro);
 characterBgImgAll = Array.prototype.slice.call(characterBgImgAll);
+cutWrapsList = Array.prototype.slice.call(cutWrapsList);
 // characterNavEls = Array.prototype.slice.call(characterNavEls);
-// cutWraps = Array.prototype.slice.call(cutWraps);
-// imgPopupEl = Array.prototype.slice.call(imgPopupEl);
-// imgPopupElImg =Array.prototype.slice.call(imgPopupElImg);
-
-
+cutWraps = Array.prototype.slice.call(cutWraps);
+cutWrapImg = Array.prototype.slice.call(cutWrapImg);
+imgPopupEl = Array.prototype.slice.call(imgPopupEl);
+imgPopupElImg =Array.prototype.slice.call(imgPopupElImg);
+console.log(imgPopupEl);
 
 for(let i =0; i< jwNavLink.length; i++){
     jwNavLink[i].addEventListener('click',(e)=>{
@@ -82,7 +84,35 @@ for(let i =0; i< jwNavLink.length; i++){
             setTimeout(()=>{
                 characterContent.classList.add('active');
             },300)
-
+            // 캐릭터 리스트 눌렀을 때
+            for(let i = 0; i < characterNavEls.length; i++){
+                characterNavEls[i].addEventListener('click',()=>{
+                    for(let j = 0; j < characterNavEls.length; j++){
+                        characterNavEls[j].classList.remove('active');
+                    }
+                    for(let i = 0; i < characterBgImgAll.length; i++){
+                        characterBgImgAll[i].classList.remove('active');
+                    }
+                    for(let i = 0; i < characterIntro.length; i++){
+                        characterIntro[i].classList.remove('active');
+                    }
+                    for(let i = 0; i < cutWrapsList.length; i++){
+                        cutWrapsList[i].classList.remove('active');
+                    }
+                    characterNavEls[i].classList.add('active');
+                    characterBgImgAll[i].classList.add('active');
+                    characterIntro[i].classList.add('active');
+                    cutWrapsList[i].classList.add('active');
+                   
+                })
+                cutWrapImg[i].addEventListener('click', ()=>{
+                    console.log('click');
+                    for(let i = 0; i < imgPopupEl.length; i++){
+                        console.log('click');
+                    }
+                })
+                
+            }
         }
         if(target === 2){
             section[0].classList.remove('active');
@@ -98,32 +128,32 @@ for(let i =0; i< jwNavLink.length; i++){
 }
 
 
-characterNavEls.forEach(characterList =>{
-    characterList.addEventListener('click',()=>{
-        characterNavEls.forEach(characterList =>{
-            characterList.classList.remove('active');
-        })
-        // cutWraps.forEach(cutWrap =>{
-        //     cutWrap.classList.remove('active');
-        // })
-        // characterBgImgAll.forEach(characterBgImg =>{
-        //     characterBgImg.classList.remove('active');
-        // })
-        for(let i = 0; i<characterBgImgAll.length; i++){
-            characterBgImgAll[i].classList.remove('active');
-        }
-        characterBgImgAll[i].classList.add('active');
+// characterNavEls.forEach(characterList =>{
+//     characterList.addEventListener('click',()=>{
+//         characterNavEls.forEach(characterList =>{
+//             characterList.classList.remove('active');
+//         })
+//         // cutWraps.forEach(cutWrap =>{
+//         //     cutWrap.classList.remove('active');
+//         // })
+//         // characterBgImgAll.forEach(characterBgImg =>{
+//         //     characterBgImg.classList.remove('active');
+//         // })
+//         for(let i = 0; i<characterBgImgAll.length; i++){
+//             characterBgImgAll[i].classList.remove('active');
+//         }
+//         characterBgImgAll[i].classList.add('active');
 
-        characterList.classList.add('active');
-        const value = characterList.textContent;
-        cutWrapImg.forEach(img =>{
-            img.style.display ="none";
-            if(img.getAttribute('data-filter') == value.toLowerCase() || value == "johnwick" ){
-                img.style.display ='block';
-            }
-        })
-    })
-})
+//         characterList.classList.add('active');
+        // const value = characterList.textContent;
+        // cutWrapImg.forEach(img =>{
+        //     img.style.display ="none";
+        //     if(img.getAttribute('data-filter') == value.toLowerCase() || value == "johnwick" ){
+        //         img.style.display ='block';
+        //     }
+        // })
+//     })
+// })
 
 // for(let i = 0; i < characterNavEls.length; i++){
 //     characterNavEls[i].addEventListener('click',(e)=>{
