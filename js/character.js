03cc01ed3009c547927be9ead92fe0d0ae8d7b2c
this-sangyeon <92290951+this-sangyeon@users.noreply.github.com>
2021-11-19 17:41:00 +0900

    btnreset

--- js/character.js
+++ js/character.js
@@ -84,10 +84,7 @@ for(let i =0; i< jwNavLink.length; i++){
             navWrap.classList.remove('active');
             characterContent.classList.remove('active');
             console.log(e.currentTarget);
-            characterIntro[0].style.visibility ="visible";
-            characterIntro[0].style.opacity = 1;
         }
-        
         if(target === 1){
             console.log(e.currentTarget);
             section[0].classList.remove('active');
