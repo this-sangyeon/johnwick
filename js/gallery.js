eaad207a6ec799af88628eeae39915c2aed3f5a5
this-sangyeon <92290951+this-sangyeon@users.noreply.github.com>
2022-01-25 21:35:37 +0900

    jw gallery mobile responsive js

--- js/gallery.js
+++ js/gallery.js
@@ -64,77 +64,110 @@ posterNextBtn.addEventListener('click',()=>{
     console.log(gallerypopUpIndex);
     BigImgPoster.setAttribute('src', galleryPopupElLink[gallerypopUpIndex])
 })
+
 posterListContainer.addEventListener('scroll',()=>{
-    value = posterListContainer.scrollTop;
-    console.log(value);
-    let screen_center= document.documentElement.scrollTop + window.innerHeight/2;
-    if(value === 0){
-        InductionText.classList.add('active');
+    if(matchMedia("screen and (min-width : 1024px)").matches){
+        value = posterListContainer.scrollTop;
+        console.log(value);
+        let screen_center= document.documentElement.scrollTop + window.innerHeight/2;
+        if(value === 0){
+            InductionText.classList.add('active');
+        }
+        if(value >= 100 && value < 200){
+            InductionText.classList.remove('active');
+        }
+        if(value < 200){
+            posterListEl[3].classList.remove('active');
+            posterListEl[4].classList.remove('active');
+            posterListEl[5].classList.remove('active');
+            posterListEl[0].classList.add('active');
+            posterListEl[1].classList.add('active');
+            posterListEl[2].classList.add('active');
+        }
+        if(value >= 600 && screen_center ){
+            posterListEl[0].classList.remove('active');
+            posterListEl[1].classList.remove('active');
+            posterListEl[2].classList.remove('active');
+            posterListEl[3].classList.add('active');
+            posterListEl[4].classList.add('active');
+            posterListEl[5].classList.add('active');
+            posterListEl[6].classList.remove('active');
+            posterListEl[7].classList.remove('active');
+            posterListEl[8].classList.remove('active');
+        }
+        if(value >= 1200 && screen_center){
+            posterListEl[3].classList.remove('active');
+            posterListEl[4].classList.remove('active');
+            posterListEl[5].classList.remove('active');
+            posterListEl[6].classList.add('active');
+            posterListEl[7].classList.add('active');
+            posterListEl[8].classList.add('active');
+            posterListEl[9].classList.remove('active');
+            posterListEl[10].classList.remove('active');
+            posterListEl[11].classList.remove('active');
+        }
+        if(value >= 2000 && screen_center){
+            posterListEl[6].classList.remove('active');
+            posterListEl[7].classList.remove('active');
+            posterListEl[8].classList.remove('active');
+            posterListEl[9].classList.add('active');
+            posterListEl[10].classList.add('active');
+            posterListEl[11].classList.add('active');
+            posterListEl[12].classList.remove('active');
+            posterListEl[13].classList.remove('active');
+            posterListEl[14].classList.remove('active');
+        }
+        if(value >= 2700  && screen_center){
+            posterListEl[9].classList.remove('active');
+            posterListEl[10].classList.remove('active');
+            posterListEl[11].classList.remove('active');
+            posterListEl[12].classList.add('active');
+            posterListEl[13].classList.add('active');
+            posterListEl[14].classList.add('active');
+        }
+        if(value >= 3300  && screen_center){
+            posterListEl[12].classList.remove('active');
+            posterListEl[13].classList.remove('active');
+            posterListEl[14].classList.remove('active');
+            posterListEl[15].classList.add('active');
+            posterListEl[16].classList.add('active');
+            posterListEl[17].classList.add('active');
+        }
+        if(value <= 3200  && screen_center){
+            posterListEl[15].classList.remove('active');
+            posterListEl[16].classList.remove('active');
+            posterListEl[17].classList.remove('active');
+        }
     }
-    if(value >= 100 && value < 200){
-        InductionText.classList.remove('active');
-    }
-    if(value < 200){
-        posterListEl[3].classList.remove('active');
-        posterListEl[4].classList.remove('active');
-        posterListEl[5].classList.remove('active');
-        posterListEl[0].classList.add('active');
-        posterListEl[1].classList.add('active');
-        posterListEl[2].classList.add('active');
-    }
-    if(value >= 600 && screen_center ){
-        posterListEl[0].classList.remove('active');
-        posterListEl[1].classList.remove('active');
-        posterListEl[2].classList.remove('active');
-        posterListEl[3].classList.add('active');
-        posterListEl[4].classList.add('active');
-        posterListEl[5].classList.add('active');
-        posterListEl[6].classList.remove('active');
-        posterListEl[7].classList.remove('active');
-        posterListEl[8].classList.remove('active');
-    }
-    if(value >= 1200 && screen_center){
-        posterListEl[3].classList.remove('active');
-        posterListEl[4].classList.remove('active');
-        posterListEl[5].classList.remove('active');
-        posterListEl[6].classList.add('active');
-        posterListEl[7].classList.add('active');
-        posterListEl[8].classList.add('active');
-        posterListEl[9].classList.remove('active');
-        posterListEl[10].classList.remove('active');
-        posterListEl[11].classList.remove('active');
-    }
-    if(value >= 2000 && screen_center){
-        posterListEl[6].classList.remove('active');
-        posterListEl[7].classList.remove('active');
-        posterListEl[8].classList.remove('active');
-        posterListEl[9].classList.add('active');
-        posterListEl[10].classList.add('active');
-        posterListEl[11].classList.add('active');
-        posterListEl[12].classList.remove('active');
-        posterListEl[13].classList.remove('active');
-        posterListEl[14].classList.remove('active');
-    }
-    if(value >= 2700  && screen_center){
-        posterListEl[9].classList.remove('active');
-        posterListEl[10].classList.remove('active');
-        posterListEl[11].classList.remove('active');
-        posterListEl[12].classList.add('active');
-        posterListEl[13].classList.add('active');
-        posterListEl[14].classList.add('active');
-    }
-    if(value >= 3300  && screen_center){
-        posterListEl[12].classList.remove('active');
-        posterListEl[13].classList.remove('active');
-        posterListEl[14].classList.remove('active');
-        posterListEl[15].classList.add('active');
-        posterListEl[16].classList.add('active');
-        posterListEl[17].classList.add('active');
-    }
-    if(value <= 3200  && screen_center){
-        posterListEl[15].classList.remove('active');
-        posterListEl[16].classList.remove('active');
-        posterListEl[17].classList.remove('active');
+    if(matchMedia("screen and (max-width : 1023px)").matches){
+        value = posterListContainer.scrollTop;
+        console.log(value);
+        if(value >= 100 && value < 500){
+            InductionText.classList.remove('active');
+        }
+        if(value >= 100 && value < 200){
+            InductionText.classList.add('active');
+        }
+        if(value >= 100 && value < 10000){
+            posterListEl[0].classList.add('active');
+            posterListEl[1].classList.add('active');
+            posterListEl[2].classList.add('active');
+            posterListEl[3].classList.add('active');
+            posterListEl[4].classList.add('active');
+            posterListEl[5].classList.add('active');
+            posterListEl[6].classList.add('active');
+            posterListEl[7].classList.add('active');
+            posterListEl[8].classList.add('active');
+            posterListEl[9].classList.add('active');
+            posterListEl[10].classList.add('active');
+            posterListEl[11].classList.add('active');
+            posterListEl[12].classList.add('active');
+            posterListEl[13].classList.add('active');
+            posterListEl[14].classList.add('active');
+            posterListEl[15].classList.add('active');
+            posterListEl[16].classList.add('active');
+            posterListEl[17].classList.add('active');
+        }
     }
 })
 function scrollToTop(){
